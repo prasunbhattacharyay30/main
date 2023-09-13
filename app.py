5ca44ca65b281e8e0db7905a314ee6b78762e1c0
Shah Rukh Rao <shahrukhrao1@.com>
2023-09-13 07:37:39 +0530

    first

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! my name</p>"
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
