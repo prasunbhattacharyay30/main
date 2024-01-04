9e416d848c788deb52c7b8b757b2064114369dc9
raoshah <shahrukhrao1@gmail.com>
2024-01-04 13:16:03 +0530

    done privacy page added

--- api/app.py
+++ api/app.py
@@ -14,6 +14,10 @@ def about():
 def canva():
     return render_template('canva.html')
 
+@app.route("/privacy")
+def privacy():
+    return render_template('privacy.html')
+
 if __name__ == '__main__':
     app.run(debug=True) 
 
